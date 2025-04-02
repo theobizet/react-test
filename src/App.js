7841eb7708861c1be65c89bed97e7c247cdcf90d
theobizet <theobizet@outlook.fr>
2025-04-02 17:04:47 +0200

    #

--- src/App.js
+++ src/App.js
@@ -1,55 +1,12 @@
 // App.js
-import React, { useState } from 'react';
 import './App.css';
-import { Navbar } from './components/NavBar';
+import { CustomNavBar } from './components/CustomNavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
-
-  const addTodo = () => {
-    if (newTodo.trim() !== '') {
-      setTodos([...todos, { text: newTodo, completed: false }]);
-      setNewTodo('');
-    }
-  };
-
-  const deleteTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
-  const toggleComplete = (index) => {
-    const newTodos = [...todos];
-    newTodos[index].completed = !newTodos[index].completed;
-    setTodos(newTodos);
-  };
-
   return (
     <div className="App">
-      <Navbar/>
-      <h1>To-Do List</h1>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Ajouter une tâche"
-      />
-      <button onClick={addTodo}>Ajouter</button>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              onChange={() => toggleComplete(index)}
-            />
-            {todo.text}
-            <button onClick={() => deleteTodo(index)}>Supprimer</button>
-          </li>
-        ))}
-      </ul>
+      <CustomNavBar />
     </div>
   );
 }
